16268a817731f037ab24d74f96a708ded887c18a
Royce <royce_nguyen@yahoo.com>
2020-10-26 11:05:18 -0400

    different ways to add button toggle

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
-// const image = document.getElementById("image");
-// let tracker = 0;
-// image.addEventListener("click", function () {
-//   var text = document.getElementById("text");
-//   if (tracker == 0) {
-//     this.src = "img/lighton.jpg";
-//     tracker = 1;
-//     text.innerHTML = Date();
-//   } else {
-//     this.src = "img/lightof.jpg";
-//     tracker = 0;
-//     text.innerHTML = "";
-//   }
+const image = document.getElementById("image");
+let tracker = 0;
+image.addEventListener("click", function () {
+  var text = document.getElementById("text");
+  if (tracker == 0) {
+    this.src = "img/lighton.jpg";
+    tracker = 1;
+    text.innerHTML = Date();
+  } else {
+    this.src = "img/lightof.jpg";
+    tracker = 0;
+    text.innerHTML = "";
+  }
+});
+
+$('.hamburger').click (function(){
+  $(this).toggleClass('active');
+});
+
+// let btns = document.querySelectorAll(".hamburger");
+// btns.forEach(btn => {
+//   btn.addEventListener("click", event => {
+//     btn.classList.toggle("active");
+// })
 // });
 
-let btns = document.getElementById("hamburger");
-btns.addEventListener("click", function () {
-  this.classList.toggle("active");
-});
+// let btns = document.querySelectorAll(".hamburger")
+// btns.forEach(function(btn){
+//   btn.addEventListener("click", function(event){
+//     btn.classList.toggle('active');
+//   })
+// })
+
+// const btns = document.getElementsByClassName('hamburger');
+// const menuToggle = function(){
+//   this.classList.toggle('active');
+// };
+// for (let i = 0; i < btns.length; i++){
+//   btns[i].addEventListener('click', menuToggle);
+// }
